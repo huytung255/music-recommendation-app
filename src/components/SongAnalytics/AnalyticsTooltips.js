93d5fc41857d7a7968aa2736c2239ec64b468ca4
huytung255 <61749223+huytung255@users.noreply.github.com>
2021-06-17 23:34:37 +0700

    update tooltip

--- src/components/SongAnalytics/AnalyticsTooltips.js
+++ src/components/SongAnalytics/AnalyticsTooltips.js
@@ -1,11 +1,13 @@
 const anlyticsTooltips = {
-  acousticness:
-    "acousticness 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but ",
-  danceability: "danceability",
-  instrumentalness: "instrumentalness",
-  energy: "energy",
-  valence: "valence",
-  speechiness: "speechiness",
-  liveness: "liveness",
+  acousticness: "The higher the value the more acoustic the song is.",
+  instrumentalness:
+    "Predicts whether a track contains no vocals. The higher the value the greater likelihood the track contains no vocal content.",
+  energy:
+    "The energy of a song - the higher the value, the more energetic the song is",
+  valence: "The higher the value, the more positive mood for the song.",
+  speechiness: "Speechiness detects the presence of spoken words in a track.",
+  danceability: "The higher the value, the easier it is to dance to this song.",
+  liveness:
+    "Higher liveness values represent an increased probability that the track was performed live.",
 };
 export default anlyticsTooltips;
