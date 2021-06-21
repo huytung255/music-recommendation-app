1bcbecd6640b15fcba68822cf13eae67968262dc
huytung255 <61749223+huytung255@users.noreply.github.com>
2021-06-21 10:44:57 +0700

    modify login/logout styles

--- src/components/Navbar/MyNavbar.js
+++ src/components/Navbar/MyNavbar.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
 import { authorize } from "../../utilfuncs/auth";
 const MyNavbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userImg, setUserImg] = useState();
   const messageFn = (event) => {
     try {
       const m = event.data;
@@ -22,11 +22,13 @@ const MyNavbar = () => {
         withCredentials: true,
       })
       .then((res) => {
+        console.log(res.data);
         if (res.data === "please login") {
           setIsLoggedIn(false);
         }
-        if (res.data === "logged in") {
+        if (res.data.status === "logged in") {
           setIsLoggedIn(true);
+          setUserImg(res.data.user_image);
         }
       })
       .catch((error) => console.log(error));
@@ -56,14 +58,31 @@ const MyNavbar = () => {
       <Navbar.Toggle className="btn-custom" aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="">
         <Nav className="mx-auto justify-content-center">
-          <Nav.Link href="/">Playlist Generator</Nav.Link>
-          <Nav.Link href="/song-analytics">Song Analytics</Nav.Link>
-          <Nav.Link href="user-analytics">User Analytics</Nav.Link>
-          <Nav.Link href="/new-releases">New releases</Nav.Link>
+          <Nav.Link href="/" className="link">
+            Playlist Generator
+          </Nav.Link>
+          <Nav.Link href="/song-analytics" className="link">
+            Song Analytics
+          </Nav.Link>
+          <Nav.Link href="user-analytics" className="link">
+            User Analytics
+          </Nav.Link>
+          <Nav.Link href="/new-releases" className="link">
+            New releases
+          </Nav.Link>
           {isLoggedIn ? (
-            <Nav.Link onClick={logout}>Log out</Nav.Link>
+            <Nav.Link onClick={logout} className="login-button">
+              <img src={userImg} alt="user" className="mr-1" /> Log out
+            </Nav.Link>
           ) : (
-            <Nav.Link onClick={login}>Log in</Nav.Link>
+            <Nav.Link onClick={login} className="login-button">
+              <img
+                src={process.env.PUBLIC_URL + "Spotify_Icon_RGB_Black.png"}
+                alt="spotify"
+                className="mr-1"
+              />{" "}
+              Log in
+            </Nav.Link>
           )}
         </Nav>
       </Navbar.Collapse>
